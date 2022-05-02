80f6146e005a0a0b2e700d69cc3340996bc1de72
scheung97 <scheung1997@gmail.com>
2022-05-02 19:49:08 -0400

    bug fixing ('Tensor' object is not callable error causing issues)

--- sp_clustering.py
+++ sp_clustering.py
@@ -7,13 +7,16 @@
 
 # Authors: Emily Costa + Spenser Cheung 
 # Created on: Apr 26, 2022
-from ipaddress import summarize_address_range
-from secrets import token_urlsafe
-from xml.etree.ElementInclude import include
+
+# from ipaddress import summarize_address_range
+# from secrets import token_urlsafe
+# from xml.etree.ElementInclude import include
+from cgi import test
 from sklearn.cluster import AgglomerativeClustering
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn import cluster as clust
 import seaborn as sns
 from scipy.spatial import distance
 from math import pi, exp, sqrt
@@ -21,9 +24,33 @@ import torch
 import hamiltorch
 from matplotlib import pyplot as plt
 from statsmodels.tsa.stattools import acf
-import pymc3 as pm3
+from torch.nn.functional import softplus
+# import pymc3 as pm3
 import arviz
 import warnings #remove later -> using to make readability of output better
+
+# select device: change to 'cpu' if there's no GPU device
+device = 'cpu'
+
+dtype = 'double'
+
+if device == 'gpu':
+    torch.cuda.set_device(0)
+    
+    if dtype=='float':
+        torch.set_default_tensor_type(torch.cuda.FloatTensor)
+        dtype_ = torch.float32
+    else:
+        torch.set_default_tensor_type(torch.cuda.DoubleTensor)
+        dtype_ = torch.double
+
+
+
+if device == 'cpu':
+    torch.set_default_tensor_type(torch.DoubleTensor)
+    dtype_ = torch.double
+hamiltorch.set_random_seed(123)
+torch.manual_seed(123)
 
 
 class Clustering:
@@ -83,9 +110,10 @@ class Clustering:
 
     def _single_point(self, point):
         data = self.data
-        # dist = data.apply(lambda x: pd.Series([self._bayesian_distance(x['Point'],point), x['Label'], x['Cluster Number']], index=['Dist', 'Label', 'Cluster Number']), axis = 1) #replace 'Point' column with 'Dist' column
-        dist = data.apply(lambda x: pd.Series([distance.euclidean(x['Point'],point), x['Label'], x['Cluster Number']], index=['Dist', 'Label', 'Cluster Number']), axis = 1) 
-        classification = data['Label'].values[dist['Dist'].idxmin()]
+        dist_2D = data.apply(lambda x: pd.Series([self._2Ddist(x['Point'],point), x['Label'], x['Cluster Number']], index=['Dist', 'Label', 'Cluster Number']), axis = 1) #Creates 2D Dist column of coord points (point0 - point1)
+        bayes_dist = self._bayesian_distance(dist_2D['Dist'])     #dist_2D.apply(lambda x: pd.Series([self._bayesian_distance(x['Dist']), x['Label'], x['Cluster Number']], index=['Dist', 'Label', 'Cluster Number']), axis = 1) #perform bayes_distance algorithm
+        # dist = data.apply(lambda x: pd.Series([self.test(x['Point'],point), x['Label'], x['Cluster Number']], index=['Dist', 'Label', 'Cluster Number']), axis = 1) 
+        classification = bayes_dist['Label'].values[bayes_dist['Dist'].idxmin()]
         
         # euclid_dist = data.apply(lambda d: distance.euclidean(d['Point'],point), axis=1) # this will be changed to Bayesian later on
         # old_dist = data.apply(lambda d: self._bayesian_distance(d['Point'],point), axis=1) #df is single column of just dist
@@ -100,12 +128,41 @@ class Clustering:
     #     probs = df.groupby('Cluster Number').size().div(len(df))
     #     return probs
 
-    def one_hot(a, num_classes): 
+    def log_prob(self, theta, K, logD_tc, D_tc, n, t = 1E-1):
+        idx = 0
+        idx1 = n*(K-1)
+        
+        W0 = theta[ idx: idx1].reshape([n,(K-1)])
+        W1 = torch.hstack([W0,torch.zeros(n,1)])
+        W = torch.softmax((W1)/t,1)
+        
+        prior_W0 =  - 1/9.0/2.0 * (W0**2).sum()
+        
+        
+        idx = idx1
+        idx1 += K
+        sigma = softplus(theta[ idx: idx1])
+        
+        idx = idx1
+        idx1 += K
+        alpha = softplus(theta[ idx: idx1])
+        
+        prior_alpha =  (- alpha).sum()
+        
+        n_h = torch.diag(W.T@W)
+        
+        prior_sigma = -1000*(sigma**2).sum()
+    
+        loglik = torch.trace(W.T@logD_tc@W*(alpha-1)/(n_h)) - torch.trace(W.T@D_tc@W/(n_h*sigma)) +  \
+            (- n_h* ( alpha* torch.log(sigma) + torch.lgamma(alpha))).sum() + prior_sigma + prior_W0 + prior_alpha
+        return loglik
+
+    def one_hot(self, a, num_classes): 
         return np.squeeze(np.eye(num_classes)[a.reshape(-1)])
 
-    def closure(self, optimizer, theta): 
+    def closure(self, optimizer, theta, K, logD_tc, D_tc, n): 
         optimizer.zero_grad()
-        loss = -self.log_prob(theta) 
+        loss = -(self.log_prob(theta, K, logD_tc, D_tc, n))
         loss.backward() 
         return loss
 
@@ -132,7 +189,12 @@ class Clustering:
         C = torch.softmax((W1)/t,1)@ np.arange(K)
         return C
     
-    def _bayesian_distance(self, dist_matrix, theta): #point0, point1):
+    def _2Ddist(self, point0, point1):
+        point0 = np.array(point0).reshape(1,2)
+        dist =  point0 - point1
+        return dist
+
+    def _bayesian_distance(self, dist_matrix): #point0, point1):
         '''
         Calculate distance to a point of all points in dataframe
 
@@ -155,11 +217,11 @@ class Clustering:
         # bayes_dist = np.linalg.norm(point0 - point1)
         
 
-        #calculations done on Gaussian "blobs" for clustering: 
-            #blobs = datasets.make_blobs(n_samples=n_samples, random_stat=0, shuffle=False)
-            #y = blobs[0]
+        # print(dist_matrix)
 
-        D = distance.squareform(distance.pdist(dist_matrix)) #input = 'blobs'
+        # dist_matrix = np.vstack(dist_matrix.values.tolist())
+        dist_matrix = np.vstack(dist_matrix.values)
+        D = distance.squareform(distance.pdist(dist_matrix)) #returns NxN marix of distances
         logD = np.log(D)
         np.fill_diagonal(logD,0)
         D_tc = torch.tensor(D)
@@ -167,43 +229,15 @@ class Clustering:
         svdD = np.linalg.svd(D) 
         p=2  
         trunc = p+2 
-        n=y.shape[0] 
-        K=32
+        n=dist_matrix.shape[0] 
+        K=3
         P=n*(K-1)+K+K  
 
-        #log_prob method
-        idx = 0
-        idx1 = n*(K-1)
-        
-        W0 = theta[ idx: idx1].reshape([n,(K-1)])
-        W1 = torch.hstack([W0,torch.zeros(n,1)])
-        W = torch.softmax((W1)/t,1)
-        
-        prior_W0 =  - 1/9.0/2.0 * (W0**2).sum()
-        
-        
-        idx = idx1
-        idx1 += K
-        sigma = torch.nn.functional.softplus(theta[ idx: idx1])
-        
-        
-        idx = idx1
-        idx1 += K
-        alpha = torch.nn.functional.softplus(theta[ idx: idx1])
-        
-        prior_alpha =  (- alpha).sum()
-        
-        n_h = torch.diag(W.T@W)
-        
-        prior_sigma = -1000*(sigma**2).sum()
-    
-        loglik = torch.trace(W.T@logD_tc@W*(alpha-1)/(n_h)) - torch.trace(W.T@D_tc@W/(n_h*sigma)) +  \
-            (- n_h* ( alpha* torch.log(sigma) + torch.lgamma(alpha))).sum() + prior_sigma + prior_W0 + prior_alpha
         
         #initialization using spectral clustering: 
-        scFit = cluster.spectral_clustering(logD -D + 1E6, n_clusters=K) 
+        scFit = clust.spectral_clustering(logD-D+1E6, n_clusters=K) 
 
-        W_init = torch(self.one_hot(scFit,3))
+        W_init = torch.tensor(self.one_hot(scFit,3))
         W_init = torch.log((W_init+0.1))
         W_init -= torch.reshape(W_init[:,K-1],[n,1])
 
@@ -211,9 +245,9 @@ class Clustering:
         theta[:(n*(K-1))] = (W_init[:,:(K-1)]).flatten()
         theta = theta.requires_grad_() 
         optimizer = torch.optim.LBFGS([theta], lr=1E-1)
-
+        
         for t in range(100): 
-            optimizer.step(self.closure(optimizer,theta))
+            optimizer.step(self.closure(optimizer, theta, K, logD_tc, D_tc, n))
         
         idx = 0
         idx1 = n*(K-1)
@@ -226,11 +260,11 @@ class Clustering:
 
         idx = idx1
         idx1 += K
-        sigma = self.softplus(theta[ idx: idx1])
+        sigma = softplus(theta[ idx: idx1])
 
         idx = idx1
         idx1 += K
-        alpha = self.softplus(theta[ idx: idx1])
+        alpha = softplus(theta[ idx: idx1])
 
         n_h = torch.diag(W.T@W)
 
@@ -238,11 +272,11 @@ class Clustering:
         
         
         #plots: 
-        plt.plot((W_np@ np.diag(np.arrange(3))).sum(1))
+        # plt.plot((W_np@ np.diag(np.arrange(3))).sum(1))
         #plt.plot(blobs[1])
-        plt.imshow((W@W.T).detach().numpy())
+        # plt.imshow((W@W.T).detach().numpy())
 
-        H = torch.autograd.functional.hessian(self.log_prob, theta).diagonal()
+        H = torch.autograd.functional.hessian(self.log_prob(theta, K, logD_tc, D_tc, n), theta).diagonal()
         inv_mass = 1.0/H.abs()
 
         #HMC NUTS
@@ -254,7 +288,7 @@ class Clustering:
         L = 100
         burn = 1000 # For results in plot burn = 2000
 
-        params_hmc_nuts = hamiltorch.sample(log_prob_func=(self.log_prob),
+        params_hmc_nuts = hamiltorch.sample(log_prob_func=(self.log_prob(theta, K, logD_tc, D_tc, n)),
                                             params_init=params_init, num_samples=num_samples,
                                             step_size=step_size, num_steps_per_sample=L,
                                             desired_accept_rate=0.6,
@@ -265,7 +299,7 @@ class Clustering:
         #extractW2: 
         param_trace = torch.vstack(params_hmc_nuts)
         trace_np = param_trace.detach().cpu().numpy()
-        plt.plot(trace_np[:,1]*1)
+        # plt.plot(trace_np[:,1]*1)
 
         theta = param_trace[1, ]
         idx = 0
@@ -275,7 +309,7 @@ class Clustering:
         W1 = torch.hstack([W0,torch.zeros(n,1)])
         W = torch.softmax((W1)/t,1)
         W_np = W.detach().numpy()
-        plt.plot((W_np@ np.diag(np.arange(3))).sum(1))
+        # plt.plot((W_np@ np.diag(np.arange(3))).sum(1))
         
         theta = param_trace[99, ]
 
@@ -290,44 +324,44 @@ class Clustering:
         W = torch.softmax((W1)/t,1)
         W_np = W.detach().numpy()
 
-        plt.plot((W_np@ np.diag(np.arange(3))).sum(1))
+        # plt.plot((W_np@ np.diag(np.arange(3))).sum(1))
         
 
         W2 = torch.zeros([n,n])
         for i in range(param_trace.shape[0]):
             W2 += self.extractW2(i,n, K,theta)
         W2_var = (W2/param_trace.shape[0])
-        plt.imshow(W2_var.detach().numpy(),vmin=0, vmax=1)
+        # plt.imshow(W2_var.detach().numpy(),vmin=0, vmax=1)
 
         C_trace = torch.stack([self.extractC(i,n,K) for i in range(2000)]).round()
 
-        acf_mat = np.vstack([statsmodel.tsa.stattools.acf(param_trace[:,i], fft=False, nlags=40) for i in range(300)])
+        acf_mat = np.vstack([self.statsmodel.tsa.stattools.acf(param_trace[:,i], fft=False, nlags=40) for i in range(300)])
         acf_mat[np.isnan(acf_mat)]=0
         acf_mat[:,0]=1
 
         ess = np.stack([arviz.ess(param_trace[:,i].numpy()) for i in range(P)])
 
-        #plots
-        fig, ax = plt.subplots(2,2, gridspec_kw={'width_ratios': [1, 1] })
-        fig.set_size_inches([8,6])
+        # #plots
+        # fig, ax = plt.subplots(2,2, gridspec_kw={'width_ratios': [1, 1] })
+        # fig.set_size_inches([8,6])
 
-        ax[0,0].plot(param_trace[:,(299)*(K-1)])
-        ax[0,0].set_title("Traceplot of $v_{300,1}$", y=-0.3)
-        ax[0,1].plot(np.arange(2000),C_trace[:,299]+1)
-        ax[0,1].set_title("Traceplot of $c_{300}$", y=-0.3)
-        ax[0,1].set_ylim([0.5,3.5])
-        ax[0,1].set_yticks([1,2,3])
+        # ax[0,0].plot(param_trace[:,(299)*(K-1)])
+        # ax[0,0].set_title("Traceplot of $v_{300,1}$", y=-0.3)
+        # ax[0,1].plot(np.arange(2000),C_trace[:,299]+1)
+        # ax[0,1].set_title("Traceplot of $c_{300}$", y=-0.3)
+        # ax[0,1].set_ylim([0.5,3.5])
+        # ax[0,1].set_yticks([1,2,3])
 
-        ax[1,0].boxplot(acf_mat[:,:40],  showfliers=False, )
-        ax[1,0].set_xticks( np.arange(6)*5+1)
-        ax[1,0].set_xticklabels(np.arange(6)*5)
-        ax[1,0].set_title("ACF of all the parameters", y=-0.3)
+        # ax[1,0].boxplot(acf_mat[:,:40],  showfliers=False, )
+        # ax[1,0].set_xticks( np.arange(6)*5+1)
+        # ax[1,0].set_xticklabels(np.arange(6)*5)
+        # ax[1,0].set_title("ACF of all the parameters", y=-0.3)
 
-        ax[1,1].boxplot(ess/2000,  showfliers=False, )
-        ax[1,1].set_xticks( [1])
-        ax[1,1].set_xticklabels([""])
-        ax[1,1].set_title("ESS per HMC iteration of all the parameters", y=-0.3)
-        fig.tight_layout(pad=1)
+        # ax[1,1].boxplot(ess/2000,  showfliers=False, )
+        # ax[1,1].set_xticks( [1])
+        # ax[1,1].set_xticklabels([""])
+        # ax[1,1].set_title("ESS per HMC iteration of all the parameters", y=-0.3)
+        # fig.tight_layout(pad=1)
         # fig.savefig("benchmark_hmc.png")
        
         idx = 0
@@ -349,26 +383,30 @@ class Clustering:
         mapC = C_trace.mode(0)[0]
         uncertainty = ((C_trace == mapC)*1.0).mean(0)
 
-        #cluster plots 
-        fig, ax = plt.subplots(1,3, gridspec_kw={'width_ratios': [1, 1,1.2] })
-        fig.set_size_inches([8,2.5])
+        # #cluster plots 
+        # fig, ax = plt.subplots(1,3, gridspec_kw={'width_ratios': [1, 1,1.2] })
+        # fig.set_size_inches([8,2.5])
 
-        ax[0].scatter(blobs[0][:,0],blobs[0][:,1], c=blobs[1], alpha=0.5, s=20)
-        ax[0].set_title("Three clusters", y= -0.4)
-
-
-        ax[1].scatter(blobs[0][:,0],blobs[0][:,1], c=mapC.numpy(), alpha=0.5, s=20, cmap='jet')
-        ax[1].set_title("Estimated  $\hat c_i$", y= -0.4)
+        # ax[0].scatter(blobs[0][:,0],blobs[0][:,1], c=blobs[1], alpha=0.5, s=20)
+        # ax[0].set_title("Three clusters", y= -0.4)
 
 
-        im1 = ax[2].scatter(blobs[0][:,0],blobs[0][:,1], c=1-uncertainty, alpha=0.5, s=20, cmap='jet', vmin=0,  vmax=0.7)
-        fig.colorbar(im1 ,ax=ax[2])
-        ax[2].set_title("Estimated uncertainty $pr(c_i \\neq \hat c_i)$", y= -0.4)
+        # ax[1].scatter(blobs[0][:,0],blobs[0][:,1], c=mapC.numpy(), alpha=0.5, s=20, cmap='jet')
+        # ax[1].set_title("Estimated  $\hat c_i$", y= -0.4)
 
 
-        fig.tight_layout(pad=1.5)
-        fig.savefig("benchmark_hmc_plot.png")
-        bayes_dist = None
+        # im1 = ax[2].scatter(blobs[0][:,0],blobs[0][:,1], c=1-uncertainty, alpha=0.5, s=20, cmap='jet', vmin=0,  vmax=0.7)
+        # fig.colorbar(im1 ,ax=ax[2])
+        # ax[2].set_title("Estimated uncertainty $pr(c_i \\neq \hat c_i)$", y= -0.4)
+
+
+        # fig.tight_layout(pad=1.5)
+        # fig.savefig("benchmark_hmc_plot.png")
+
+
+        bayes_dist = mapC.numpy() 
+        print(bayes_dist)
+        uncertainty = 1-uncertainty 
         return bayes_dist
 
 if __name__=="__main__":
