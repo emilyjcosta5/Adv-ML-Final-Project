e5ae6bb4518c45a0bc2ad023a4beaf88b967f73f
henrywchuang <65669603+henrywchuang@users.noreply.github.com>
2022-05-04 11:19:27 -0400

    Update experiments.py

--- experiments.py
+++ experiments.py
@@ -38,7 +38,7 @@ def linear_equally_spaced_clusters(n_points=100, proportion=(1,1), n_clusters=2)
         # generate clusters for this particular distance
         for i in range(1,n_clusters):
             mean = (0,i*x)
-            cov = ((1,0),(0,1))
+            cov = ((0.1,0),(0,0.1))
             size = int(n_points*proportion[i]/sum(proportion))
             index_values = [i for x in range(size)]
             cluster = np.random.multivariate_normal(mean=mean, cov=cov, size=size)
@@ -64,7 +64,7 @@ def linear_equally_spaced_clusters(n_points=100, proportion=(1,1), n_clusters=2)
     print(bayesian_accuracies)
     return euclidean_accuracies, bayesian_accuracies
 
-def radial_equally_spaced_clusters(n_points=100, proportion=(1,1,1,1), n_clusters=4):
+def radial_equally_spaced_clusters(n_points=200, proportion=(1,1,1,1), n_clusters=4):
     '''
     Like linear_equally_spaced_clusters, but all the clusters are evenly placed in a circle
     around the origin
@@ -122,10 +122,10 @@ def radial_equally_spaced_clusters(n_points=100, proportion=(1,1,1,1), n_cluster
     print(bayesian_accuracies)
     return euclidean_accuracies, bayesian_accuracies
 
-def vertically_elongated_equally_spaced_clusters(n_points=100, proportion=(1,1,1), n_clusters=3):
+def two_equal_clusters_additional_points():
     '''
-    Like linear_equally_spaced_clusters, but the clusters are elongated orthogonal to their separation
-    instead of their gap increasing
+    Like linear_equally_spaced_clusters, but all the clusters are evenly placed in a circle
+    around the origin
 
     Parameters
     ----------
@@ -140,26 +140,30 @@ def vertically_elongated_equally_spaced_clusters(n_points=100, proportion=(1,1,1
     euclidean_accuracies = []
     bayesian_accuracies = []
     column_values = ["x","y"]
+    pi = math.pi
     mean = (0,0)
     cov = ((1,0),(0,1))
-    size = int(n_points*proportion[0]/sum(proportion))
+    size = 50
     cluster_number = 0
+    n_clusters = 2
     index_values = [cluster_number for x in range(size)]
     cluster = np.random.multivariate_normal(mean=mean, cov=cov, size=size)
     df = pd.DataFrame(data=cluster, index=index_values, columns=column_values)
-    distances = np.linspace(0., 10., num=20)
-    for x in distances:
+    vals = list(range(0,20))
+    data_holder = df.copy()
+    for x in vals:
         tmp = df.copy()
         # generate clusters for this particular distance
         for i in range(1,n_clusters):
-            mean = (0,i)
-            cov = ((x,0),(0,1))
-            size = int(n_points*proportion[i]/sum(proportion))
-            index_values = [i for x in range(size)]
+            mean = (0,i*5)
+            cov = ((1,0),(0,1))
+            size = 50
+            index_values = [i+x*50 for x in range(size)]
             cluster = np.random.multivariate_normal(mean=mean, cov=cov, size=size)
-            tmp = tmp.append(pd.DataFrame(data=cluster, index=index_values, columns=column_values))
+            data_holder = data_holder.append(pd.DataFrame(data=cluster, index=index_values, columns=column_values))
+        print(data_holder)
         # do initial clustering and check accuracy of clustering methodology
-        clustering = Clustering(tmp, n_clusters)
+        clustering = Clustering(data_holder, n_clusters)
         tmp = clustering.get_clustered_data()
         euclidean_accuracy = np.abs(tmp['Label'].corr(tmp['Cluster Number'], method='spearman'))
         euclidean_accuracies.append(euclidean_accuracy)
@@ -167,8 +171,8 @@ def vertically_elongated_equally_spaced_clusters(n_points=100, proportion=(1,1,1
         points = pd.DataFrame(columns=column_values + ['Label', 'Cluster Number'])
         for p in range(100):
             for i in range(0,n_clusters):
-                mean = (0,i)
-                cov = ((x,0),(0,1))
+                mean = (math.cos(i*pi/n_clusters)*x,math.sin(i*pi/n_clusters)*x)
+                cov = ((1,0),(0,1))
                 point = np.random.multivariate_normal(mean=mean, cov=cov, size=1)[0]
                 classification = clustering.add_point(point)
                 points.loc[len(points.index)] = [point[0], point[1], classification, i]
@@ -179,18 +183,17 @@ def vertically_elongated_equally_spaced_clusters(n_points=100, proportion=(1,1,1
     print(bayesian_accuracies)
     return euclidean_accuracies, bayesian_accuracies
 
-
 if __name__=="__main__":
     distances = list(range(1,20+1))
-    scenario = linear_equally_spaced_clusters()
-    plt.plot(distances,scenario[0],color='r',label='Euclidean Accuracy')
-    plt.plot(distances,scenario[1],color='b',label='Bayesian Accuracy')
-    plt.xlabel('Distance between clusters')
+    scenario = two_equal_clusters_additional_points()
+    plt.plot(distances,scenario[0],color='b',label='Euclidean Accuracy')
+#    plt.plot(distances,scenario[1],color='b',label='Bayesian Accuracy')
+    plt.xlabel('Additional data')
     plt.ylabel('Accuracy (%)')
     plt.legend()
     plt.xlim([1,20])
     plt.ylim([0,1])
-    plt.title('3 Linear Equally Spaced Gaussian clusters')
+    plt.title('2 equally spaced Gaussian clusters with points gradually added')
 
     plt.show()
 
